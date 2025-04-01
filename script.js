77405c3b7509bc678df9f3724936a5ed055b33d0
Jitendra Kumar Chauhan <jitendra.chauhan@abes.ac.in>
2025-04-01 10:26:29 +0530

    first advanced js code

---
+++ script.js
@@ -0,0 +1,14 @@
+// function sayHello() {
+//     let you = prompt("What's your name? ");
+//     console.log("Hello", you + "!");
+//    }
+
+//    sayHello();
+
+
+   function tester(para1, para2){
+    return para1 + " " + para2;
+   }
+   const arg1 = "argument 1";
+   const arg2 = "argument 2";
+  console.log( tester(arg1, arg2));
